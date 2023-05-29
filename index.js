0fc8da925163838b9ce360592bb73001b67710aa
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-29 17:58:23 +0600

    delete operation is added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znucezt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,7 @@ async function run() {
 
     const menuCollection = client.db("BistroDB").collection("menu")
     const reviewCollection = client.db("BistroDB").collection("reviews")
+    const cartCollection = client.db("BistroDB").collection("carts")
 
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray()
@@ -41,6 +42,32 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result)
     })
+
+    // cart collection apis
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email
+      console.log(email)
+      if(!email){
+        res.send([])
+      }
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post('/carts', async(req, res) => {
+      const item = req.body
+      console.log(item)
+      const result = await cartCollection.insertOne(item)
+      res.send(result)
+    })
+
+   app.delete('/carts/:id', async(req, res) => {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await cartCollection.deleteOne(query)
+    res.send(result)
+   })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -63,3 +90,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro boss is sitting on port ${port}`)
 })
+
+/**
+ * ==============================
+ * ###### NAMING CONVENTION #####
+ * ==============================
+ * users: userCollection      => get user collection
+ * app.get('/users')          => get all users
+ * app.get('/users/:id')      => single user
+ * app.post('users')          => create user
+ * app.patch('/users/:id')    => update single user
+ * app.put('/users/:id')      => update single user
+ * app.delete('/users/:id')   => delete single user
+ * 
+ * 
+*/
